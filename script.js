0d156c99f50b7d813723d2646858b08227e75beb
danielearmilla-dotcom <danielearmilla@gmail.com>
2026-07-29 13:46:09 +0200

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-script.js
+const players = document.querySelectorAll("audio");
+
+players.forEach(player => {
+
+    player.addEventListener("play", () => {
+
+        players.forEach(audio => {
+
+            if(audio !== player){
+                audio.pause();
+                audio.currentTime = 0;
+            }
+
+        });
+
+    });
+
+});
